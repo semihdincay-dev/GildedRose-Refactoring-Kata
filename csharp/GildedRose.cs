4a14d04ceba43c87c79fab3666e919dc5d95b124
Semih Dinçay <semihdincay@gmail.com>
2022-07-28 00:06:27 +0300

    Yarle Programming Challange GildedRose Refactoring

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -2,88 +2,83 @@
 
 namespace csharp
 {
-    public class GildedRose
+  public class GildedRose
+  {
+    IList<Item> Items;
+    public GildedRose(IList<Item> Items)
     {
-        IList<Item> Items;
-        public GildedRose(IList<Item> Items)
-        {
-            this.Items = Items;
-        }
-
-        public void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
-        }
+      this.Items = Items;
     }
+
+    public void UpdateQuality()
+    {
+      Item item;
+      for (var i = 0; i < Items.Count; i++)
+      {
+        item = Items[i];
+
+        if (item.Name == "Sulfuras, Hand of Ragnaros")
+          continue;
+
+        if (item.Quality > 0 && item.Quality < 50)
+        {
+          switch (item.Name)
+          {
+            case "Aged Brie":
+              UpdateAgedBrieGoods(item);
+              break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+              UpdateBackstage(item);
+              break;
+            case "Conjured Mana Cake":
+              UpdateConjuredGoods(item);
+              break;
+            default:
+              UpdateNormalGoods(item);
+              break;
+          }
+          item.SellIn = item.SellIn - 1;
+        }
+
+      }
+    }
+    public void UpdateNormalGoods(Item item)
+    {
+      if (item.SellIn > 0)
+      {
+        item.Quality = item.Quality - 1;
+      }
+      else if (item.Quality > 0 && item.SellIn < 0)
+      {
+        item.Quality = item.Quality - 2;
+      }
+    }
+
+    public void UpdateConjuredGoods(Item item)
+    {
+      if (item.Quality > 1)
+        item.Quality = item.Quality - 2;
+    }
+
+    public void UpdateAgedBrieGoods(Item item)
+    {
+      item.Quality = item.Quality + 1;
+    }
+
+    public void UpdateBackstage(Item item)
+    {
+      if (item.SellIn < 6)
+      {
+        item.Quality = item.Quality + 3;
+      }
+      else if (item.SellIn < 11)
+      {
+        item.Quality = item.Quality + 2;
+      }
+      else if (item.SellIn < 0)
+      {
+        item.Quality = 0;
+      }
+    }
+  }
 }
--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -1,18 +1,195 @@
 ﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace csharp
 {
-    [TestFixture]
-    public class GildedRoseTest
+  [TestFixture]
+  public class GildedRoseTest
+  {
+    [Test]
+    public void Should_QualityIsNotNegative_When_UpdateQuality_IsCalled()
     {
-        [Test]
-        public void foo()
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "foo", SellIn = 0, Quality = 0 } ,
+              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+              new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15,Quality = 20},
+              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 15,Quality = 20},
+              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+    };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+          Console.WriteLine(Items[j]);
         }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+
+      Assert.AreNotEqual(-1, Items[0].Quality);
+      Assert.AreNotEqual(-1, Items[1].Quality);
+      Assert.AreNotEqual(-1, Items[2].Quality);
+      Assert.AreNotEqual(-1, Items[3].Quality);
+      Assert.AreNotEqual(-1, Items[4].Quality);
+      Assert.AreNotEqual(-1, Items[5].Quality);
+      Assert.AreNotEqual(-1, Items[6].Quality);
     }
+
+    [Test]
+    public void Should_SellIn_When_UpdateQuality_IsCalled()
+    {
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "foo", SellIn = 0, Quality = 0 } ,
+            };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
+        {
+          Console.WriteLine(Items[j]);
+        }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+      Assert.AreNotEqual(-1, Items[0].Quality);
+    }
+
+    [Test]
+    public void Should_SellIn_IsNegative_When_UpdateQuality_IsCalled()
+    {
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "foo", SellIn = -1, Quality = 10 } ,
+            };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
+        {
+          Console.WriteLine(Items[j]);
+        }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+      Assert.AreNotEqual(-1, Items[0].SellIn);
+    }
+
+
+    [Test]
+    public void Should_NormalGoods_AreUpdated_When_UpdateQuality_IsCalled()
+    {
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+            };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
+        {
+          Console.WriteLine(Items[j]);
+        }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+      Assert.AreEqual("+5 Dexterity Vest", Items[0].Name);
+    }
+
+    [Test]
+    public void Should_AgedBrie_AreUpdated_When_UpdateQuality_IsCalled()
+    {
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "Aged Brie", SellIn = 10, Quality = 20},
+            };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
+        {
+          Console.WriteLine(Items[j]);
+        }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+      Assert.AreEqual("Aged Brie", Items[0].Name);
+    }
+
+    [Test]
+    public void Should_Backstage_AreUpdated_When_UpdateQuality_IsCalled()
+    {
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 20},
+            };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
+        {
+          Console.WriteLine(Items[j]);
+        }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+      Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", Items[0].Name);
+    }
+
+    [Test]
+    public void Should_Conjured_AreUpdated_When_UpdateQuality_IsCalled()
+    {
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "Conjured Mana Cake", SellIn = 10, Quality = 20},
+            };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
+        {
+          Console.WriteLine(Items[j]);
+        }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+      Assert.AreEqual("Conjured Mana Cake", Items[0].Name);
+    }
+
+    [Test]
+    public void Should_Sulfuras_AreUpdated_When_UpdateQuality_IsCalled()
+    {
+      IList<Item> Items = new List<Item> {
+              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 20},
+            };
+      GildedRose app = new GildedRose(Items);
+      for (var i = 0; i < 31; i++)
+      {
+        Console.WriteLine("-------- day " + i + " --------");
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < Items.Count; j++)
+        {
+          Console.WriteLine(Items[j]);
+        }
+        Console.WriteLine("");
+        app.UpdateQuality();
+      }
+      Assert.AreEqual("Sulfuras, Hand of Ragnaros", Items[0].Name);
+    }
+
+    
+
+  }
 }
